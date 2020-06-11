45e7fcd75b1c41102dbd2b15fe94f3f8f04ee34f
Oleksandr Ocheretnyi <ochierietnii@gmail.com>
2020-06-11 16:11:45 +0300

    updated example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular2-feedback-project';
+
+  onFeedbackFinished($event) {
+    console.log($event);
+  }
 }
